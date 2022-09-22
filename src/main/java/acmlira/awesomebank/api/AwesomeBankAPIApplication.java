163a61751b12eeab062116a6cb8c4eefcc685aa5
Allan C. M. Lira <acmlira@gmail.com>
2022-09-21 22:04:46 -0300

    Initializr files + infrastructure

---
+++ src/main/java/acmlira/awesomebank/api/AwesomeBankAPIApplication.java
@@ -0,0 +1,13 @@
+package acmlira.awesomebank.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeBankAPIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwesomeBankAPIApplication.class, args);
+    }
+
+}
